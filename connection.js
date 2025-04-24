495cd79ffe8a1ac8ec9acec6d844504914f46c63
Backend Developer <backend.developer.profilics@gmail.com>
2025-04-24 06:59:40 -0400

    fix an error in mongoDB connection

--- connection.js
+++ connection.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose');
 
-
-async function connectMongoDB(url) {
-return mongoose.connect(url);
-}
-
+const connectMongoDB = async (url) => {
+  try {
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
+};
 
 module.exports = connectMongoDB;
 
